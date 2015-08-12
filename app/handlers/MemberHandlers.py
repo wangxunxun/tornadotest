25e8f4ca11a419c21053ef1a3a112bd0360211de
troywang <troy.wang@beyondsoft.com>
2015-08-12 23:26:12 +0800

    4545445

--- app/handlers/MemberHandlers.py
+++ app/handlers/MemberHandlers.py
@@ -33,93 +33,47 @@ class memberManageHandler(BaseHandler):
         else:
             self.render("addteam.html",  bodytitle = "添加小组",error = "该小组已添加，不能重复添加")
 
-class addMemberEmailHandler(BaseHandler):
-    
-    def get(self):
-        self.render("addmember_email.html",  bodytitle = "添加成员", error = "")
-    def post(self):
-        token = MakeToken.Token()
-        email = self.get_argument("email")
-        name = self.get_argument("name")
-
-        
-
-        if not self.session.query(Member).filter(Member.email==email).scalar():
-            member1 = Member(email = email,name = name)
-            self.session.add(member1)
-            self.session.commit()
-            user = self.session.query(Member).filter(Member.email==email).scalar()   
-            newtoken = str(token.generate_newmember_token(user.id,email, name))
-            newtoken = newtoken[2:len(newtoken)-1]       
-            self.redirect("/addmemberteam/"+newtoken)
-        else:
-            self.render("addmember_email.html",  bodytitle = "添加成员",error = "该邮箱已注册")
-                            
 
             
-class addMemberTeamHandler(BaseHandler):
+class addMemberHandler(BaseHandler):
     
-    def get(self,input):
-        token = MakeToken.Token()
-        data = token.newmember_token(input)        
-        email = data.get("email")
-        name = data.get("name")
-        id = data.get('id')
+    def get(self):
         allteams = self.session.query(Team).all()
-        print(self.session.query(Member).filter(Member.id == id).scalar())
-        savedteams = self.session.query(Member).filter(Member.id == id).scalar().teams.all()
-        savedteamsid = []
-        for t in savedteams:
-            savedteamsid.append(t.teamid)             
-        nosavedteam = []
-        for i in allteams:
-            if i.id not in savedteamsid:
-                nosavedteam.append(i)
-              
-        if self.session.query(Member).filter(Member.email ==email).scalar():
-            self.render("addmember_team.html", email = email,name = name,
-                        bodytitle = "添加小组", error = "",teams = nosavedteam)
-        else:
-            self.redirect("/404")
+        self.render("addmember.html", bodytitle = "添加小组", error = "",teams = allteams)
+
    
-    def post(self,input):
-        token = MakeToken.Token()
-        data = token.newmember_token(input)        
-        email = data.get("email")
-        name = data.get("name")
-        id = data.get('id')
+    def post(self):
+        email = self.get_argument("email")
+        name = self.get_argument("name")
         allteams = self.session.query(Team).all()
-        savedteams = self.session.query(Member).filter(Member.id == id).scalar().teams.all()
-        savedteamsid = []
-        for t in savedteams:
-            savedteamsid.append(t.teamid)             
-        nosavedteam = []
-        for i in allteams:
-            if i.id not in savedteamsid:
-                nosavedteam.append(i)
-        
-        
-                       
-        chooseteams =self.get_arguments("teams")        
-        i = 1
+        chooseteams =self.get_arguments("teams") 
         teamsid = []
         for i in chooseteams:
             que = self.session.query(Team).filter(Team.name ==i).scalar()
             teamsid.append(que.id)
+        
+        if not self.session.query(Member).filter(Member.email==email).scalar():
+            member1 = Member(email = email,name = name)
+            self.session.add(member1)
+            self.session.commit()
+            id = self.session.query(Member).filter(Member.email == email).scalar().id
+            if teamsid:
+                i = 0
+                while i<len(teamsid):
+                    tm = Team_member(teamid =teamsid[i],teamname = chooseteams[i],memberid = id,
+                                     memberemail = email)
+                    self.session.add(tm)
+                    i = i+1
+                    
 
-        if teamsid:
-            i = 0
-            while i<len(teamsid):
-                tm = Team_member(teamid =teamsid[i],teamname = chooseteams[i],memberid = id,
-                                 memberemail = email)
-                self.session.add(tm)
                 self.session.commit()
-                i = i+1
-            self.redirect("/membermanage")
-                                        
+                self.redirect("/membermanage")
+                                            
+            else:
+                self.render("addmember.html",  bodytitle = "添加小组",error = "请选择小组",teams = allteams)
         else:
-            self.render("addmember_team.html",  bodytitle = "添加小组",email = email,
-                        name = name,error = "请选择小组",teams = nosavedteam)
+            self.render("addmember.html",  bodytitle = "添加成员",error = "该邮箱已注册",teams = allteams)
+
 
 class editMemberHandler(BaseHandler):
     
